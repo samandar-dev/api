df480121762aec1118d0dc30dda761b7aee36ce5
samandar-dev <samandarbohodirov01@gmail.com>
2022-04-18 22:25:43 +0500

    add

--- js/main.js
+++ js/main.js
@@ -1,11 +1,80 @@
-// let elMenuBtn = document.getElementById('menuBtn');
-// let elMenuBox = document.getElementById('menuBox');
-// let elMenuRemov = document.getElementById('menuRemov')
+let list = document.querySelector('.data__list');
+let arr = []
 
-// elMenuBtn.addEventListener('click', function () {
-//   elMenuBox.classList.add('open-menu');
-// })
+function dataFunc() {
+  let requst = new XMLHttpRequest();
+  let url = "https://reqres.in/api/users"
 
-// elMenuRemov.addEventListener('click', function () {
-//   elMenuBox.classList.remove('open-menu');
-// })
+  requst.open("GET", url);
+
+  requst.onload = function () {
+    let outData = JSON.parse(requst.responseText);
+    renderHTML(outData);
+  }
+
+  requst.send();
+} dataFunc()
+
+
+function renderHTML(data) {
+  data.data.forEach(element => {
+    let li = document.createElement('li')
+    li.className = "data__item"
+    li.innerHTML = `
+            <div class="data__item_box itmbox">
+              <div class="itmbox__img_name">
+                <div class="itmbox__img-box">
+                  <img class="itmbox__img" src="${element.avatar}" alt="img">
+                </div>
+
+                <div class="itmbox__name-box">
+                  <p class="itmbox__name">${element.last_name}</p>
+                  <p class="itmbox__name-tit">${element.first_name}</p>
+                </div>
+              </div>
+
+              <div class="itmbox__content">
+                <ul class="itmbox__left_list">
+                  <li class="itmbox__left_item">
+                    <p class="itmbox__left_tit">Company</p>
+                    <p class="itmbox__right_tit">Epam</p>
+                  </li>
+                  <li class="itmbox__left_item">
+                    <p class="itmbox__left_tit">Email</p>
+                    <p class="itmbox__right_tit">${element.email}</p>
+                  </li>
+                  <li class="itmbox__left_item">
+                    <p class="itmbox__left_tit">Phone</p>
+                    <p class="itmbox__right_tit">999-876-654-321</p>
+                  </li>
+                  <li class="itmbox__left_item">
+                    <p class="itmbox__left_tit">Website</p>
+                    <p class="itmbox__right_tit">kale.biz</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <button class="removBtn">Remove</button>`;
+
+    list.appendChild(li)
+    arr.push(element)
+  });
+
+  let items = document.querySelectorAll('.data__item');
+  let btns = document.querySelectorAll('.removBtn');
+  items.forEach((item, inx) => {
+    btns.forEach((btn, btninx) => {
+      btn.addEventListener('click', () => {
+        if (btninx + 1 == inx + 1) {
+          console.log(item);
+          item.className = 'remov-anim';
+          item.classList.add('width-anim');
+          setTimeout(() => {
+            item.remove();
+          }, 750)
+        }
+      })
+    })
+  })
+}
+
